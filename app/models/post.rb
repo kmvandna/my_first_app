9c67bc6e42c1ea783d6841eda48e796bebd2d38b
kmvandna <vandna_gola@qwertycode.in>
2022-05-22 18:41:26 +0530

    like

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+  has_many :likes, as: :liked_to, dependent: :destroy
+
+  def is_liked_by_user?(user_id)
+    self.likes.find_by_user_id(user_id).present?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "posts#index"
-  post "like/:record_type/:record_id", to: "likes#like_toggle", as: "like_record"
-
+  
   resources :posts do
+   post "like/:record_type/:record_id", to: "likes#like_toggle", as: "like_record"
   # Defines the root path route ("/")
   end
 end
