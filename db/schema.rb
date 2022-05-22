bb633c7d007aae4a442d05e66b3f496f903ad98f
kmvandna <vandna_gola@qwertycode.in>
2022-05-22 18:12:13 +0530

    create Like

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_21_200721) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_22_123331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "liked_to_type", null: false
+    t.integer "liked_to_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["liked_to_type", "liked_to_id"], name: "index_likes_on_liked_to_type_and_liked_to_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
